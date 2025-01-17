3db0245e73f9ffa416f0a3b3308c8444da61285f
Nicolas Bueno <nicolasbrian202@gmail.com>
2025-01-16 22:21:40 -0300

    greeting service

---
+++ 01-greeting-service/greeting-service/src/main/java/com/nb/configuration/GreetingConfiguration.java
@@ -0,0 +1,31 @@
+package com.nb.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("greeting-service")
+public class GreetingConfiguration {
+
+    private String greeting;
+    private String defaultValue;
+
+    public GreetingConfiguration() {}
+
+    public String getGreeting() {
+        return this.greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getDefaultValue() {
+        return this.defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+    
+}
