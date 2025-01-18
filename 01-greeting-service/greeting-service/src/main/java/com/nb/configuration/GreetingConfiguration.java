989457320fa3198a6a306fdcc5b109f0eff2d1fe
Nicolas Bueno <nicolasbrian202@gmail.com>
2025-01-18 16:39:46 -0300

    integrando spring cloud config server com o github

--- 01-greeting-service/greeting-service/src/main/java/com/nb/configuration/GreetingConfiguration.java
+++ 01-greeting-service/greeting-service/src/main/java/com/nb/configuration/GreetingConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("greeting-service")
+@ConfigurationProperties(prefix = "greeting-service")
 public class GreetingConfiguration {
 
     private String greeting;
