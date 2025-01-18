01cd604b5385b4cabf2bb24f43a2d4bb1f9e0431
Nicolas Bueno <nicolasbrian202@gmail.com>
2025-01-18 19:46:00 -0300

    atualizando as config sem interromper o microsserviço

--- 01-greeting-service/greeting-service/src/main/java/com/nb/configuration/GreetingConfiguration.java
+++ 01-greeting-service/greeting-service/src/main/java/com/nb/configuration/GreetingConfiguration.java
@@ -1,9 +1,11 @@
 package com.nb.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties(prefix = "greeting-service")
 public class GreetingConfiguration {
 
