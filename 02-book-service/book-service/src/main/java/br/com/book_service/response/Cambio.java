bc2a7ac500752d67025d291230e8dcf11fcf4274
Nicolas Bueno <nicolasbrian202@gmail.com>
2025-01-26 21:08:09 -0300

    consumindo o endpoint de cambio service no book service

---
+++ 02-book-service/book-service/src/main/java/br/com/book_service/response/Cambio.java
@@ -0,0 +1,74 @@
+package br.com.book_service.response;
+
+public class Cambio{
+    
+    private Long id;
+    private String from;
+    private String to;
+    private Double conversionFactor;
+    private Double convertedValue;
+    private String environment;
+
+    public Cambio() {}
+
+
+    public Cambio(Long id, String from, String to, Double conversionFactor, Double convertedValue, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionFactor = conversionFactor;
+        this.convertedValue = convertedValue;
+        this.environment = environment;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public Double getConversionFactor() {
+        return this.conversionFactor;
+    }
+
+    public void setConversionFactor(Double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public Double getConvertedValue() {
+        return this.convertedValue;
+    }
+
+    public void setConvertedValue(Double convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getEnvironment() {
+        return this.environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+    
+
+}
