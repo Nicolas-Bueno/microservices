fc5aee4107530b7eea412fb7dfad2dda45d906f9
Nicolas Bueno <nicolasbrian202@gmail.com>
2025-02-01 15:28:46 -0300

    circuito breaker do resilience4j

--- 02-book-service/book-service/src/main/java/br/com/book_service/controller/FooBarController.java
+++ 02-book-service/book-service/src/main/java/br/com/book_service/controller/FooBarController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -12,7 +13,8 @@ import io.github.resilience4j.retry.annotation.Retry;
 public class FooBarController {
 
     @GetMapping("/foo-bar")
-    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+    //@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
     public String fooBar(){
         new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
         return "Foo-Bar!!!";
