dd36d3203049ba86932879758d6061024881fdf3
Nicolas Bueno <nicolasbrian202@gmail.com>
2025-02-01 15:21:08 -0300

    fallbackMethod com Resilience4j Wait duration e exponencial backoff

--- 02-book-service/book-service/src/main/java/br/com/book_service/controller/FooBarController.java
+++ 02-book-service/book-service/src/main/java/br/com/book_service/controller/FooBarController.java
@@ -3,13 +3,22 @@ package br.com.book_service.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
 
     @GetMapping("/foo-bar")
+    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
     public String fooBar(){
+        new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
         return "Foo-Bar!!!";
+    }
+
+    public String fallbackMethod(Exception ex){
+        return "fallbackMethod foo-bar!!!";
     }
 }
